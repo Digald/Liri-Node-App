eb99a8363a2bbe118b066e5fe2062c48fa54af9e
Digald <nosracer_gt@sbcglobal.net>
2018-01-06 01:33:22 -0600

    added require for keys.js

--- liri.js
+++ liri.js
@@ -1,12 +1,18 @@
+// Message to notify that the script is running
+console.log("Liri is listening");
 // Read and set any enviornment variables with the dotenv package
 require("dotenv").config();
+var keys = require('./keys.js');
 var Twitter = require('twitter');
 // Add code required to import the keys.js file and store it in a variable
 // var spotify = new Spotify(keys.spotify);
 var client = new Twitter(Twitter.twitter);
-console.log(client.get);
+// console.log(client);
+
 // if (process.argv[2] === "my-tweets") {
-client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response) {
+client.get('search/tweets', {
+  q: 'node.js'
+}, function(error, tweets, response) {
   console.log(tweets);
 });
 // }
