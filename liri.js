e30c331de7bee9a71e36a7e91593b0cdf03c2d6d
Digald <nosracer_gt@sbcglobal.net>
2018-01-05 22:44:56 -0600

    installed all npm packages and attached twitter keys

--- liri.js
+++ liri.js
@@ -1,5 +1,17 @@
 // Read and set any enviornment variables with the dotenv package
 require("dotenv").config();
+var Twitter = require('twitter');
 // Add code required to import the keys.js file and store it in a variable
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+// var spotify = new Spotify(keys.spotify);
+var client = new Twitter(Twitter.twitter);
+console.log(client);
+// if (process.argv[2] === "my-tweets") {
+client.stream('statuses/filter', {track:'twitter'}, function(stream) {
+  stream.on('data', function(tweet) {
+    console.log(tweet.text);
+  });
+  stream.on('error', function(error) {
+    console.log(error);
+  });
+});
+// }
