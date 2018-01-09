9b3642d5c36ea789373887e450fdf894c851e1d0
Digald <nosracer_gt@sbcglobal.net>
2018-01-08 18:26:18 -0600

    Twitter portion is working now

--- liri.js
+++ liri.js
@@ -1,14 +1,15 @@
 // Message to notify that the script is running
 console.log("Liri is listening");
-// Read and set any enviornment variables with the dotenv package
+// Importing packages for use and keys
 require("dotenv").config();
 var keys = require('./keys.js');
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
-var spotify = new Spotify(Spotify.spotify);
-var client = new Twitter(Twitter.twitter);
 var request = require("request");
-// --------------------------------------Twitter functionality
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+// -------------------------------------Twitter functionality
+
 // var params = {
 //   screen_name: 'BillNye'
 // };
@@ -27,21 +28,21 @@ var request = require("request");
 // });
 // -------------------------------------Spotify functionality
 
-// spotify.search({
-//   type: 'track',
-//   query: 'All the Small Things',
-//   limit: 1
-// }, function(err, data) {
-//   if (err) {
-//     return console.log('Error occurred: ' + err);
-//   }
-//   var info = data.tracks.items[0];
-//   // console.log(info);
-//   console.log("Artists: " + info.artists[0].name);
-//   console.log("Track: " + info.name);
-//   console.log("Preview Song: " + info.external_urls.spotify);
-//   console.log("Album: " + info.album.name);
-// });
+spotify.search({
+  type: 'track',
+  query: 'All the Small Things',
+  limit: 1
+}, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+  var info = data.tracks.items[0];
+  // console.log(info);
+  console.log("Artists: " + info.artists[0].name);
+  console.log("Track: " + info.name);
+  console.log("Preview Song: " + info.external_urls.spotify);
+  console.log("Album: " + info.album.name);
+});
 
 // ----------------------------------------OMDB functionality
 // var movieName = "pokemon";
